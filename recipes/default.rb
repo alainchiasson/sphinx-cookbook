72c5b697c1718edd67533e7f364e87031ce332f3
Tom Wilson <tom@rollnorocks.com>
2015-04-13 12:32:51 -0600

    mysql::client is not essential, and recipe does not exist for mysql cookbook > 4.0

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,4 +1,5 @@
 include_recipe "percona::client" if node[:sphinx][:use_percona]
-include_recipe "mysql::client" if node[:sphinx][:use_mysql]
+# next line is not essential, and fails for mysql > 4.0
+# include_recipe "mysql::client" if node[:sphinx][:use_mysql]
 include_recipe "postgresql::client" if node[:sphinx][:use_postgres]
 include_recipe "sphinx::#{node[:sphinx][:install_method]}"
